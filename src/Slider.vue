a24b31220ad60d69f4a484947db3f3ccfc326f2b
Rio <532903866@qq.com>
2021-01-23 12:42:31 +0800

    init

---
+++ src/Slider.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="vertical==true?'slider-h':'slider-v'">
+    <div class="process">
+      x
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    vertical:{
+      type:Boolean,
+      default:()=>{
+        return false
+      }
+    }
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.slider-h{
+  width: 38px;
+  height: 200px;
+  box-sizing: border-box;
+}
+.slider-v{
+  width: 200px;
+  height: 38px;
+  box-sizing: border-box;
+}
+.process{
+  width: 5px;
+  height: 100%;
+  padding: 20px 0;
+}
+</style>
---
+++ test/test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="test">
+    <slider 
+      class="test-color"
+      :vertical="true"
+    >
+
+    </slider>
+  </div>
+</template>
+
+<script>
+import Slider from '../src/Slider.vue';
+export default {
+  components:{
+    Slider
+  }
+}
+</script>
+
+<style scoped>
+.test{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+}
+.test-color{
+  border: 1px solid #24ccff;
+}
+</style>
